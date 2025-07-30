f74aefc33ceca65f5c6445945e4cddc459bb4578
JunYong An <happihanjy@naver.com>
2025-07-30 21:38:12 +0900

    [refactor] Add year and semester selection options

--- src/sections/mileage-scholarship-application/dynamic-form.tsx
+++ src/sections/mileage-scholarship-application/dynamic-form.tsx
@@ -6,6 +6,26 @@ import { Stack, MenuItem, TextField } from '@mui/material';
 
 import { MileageArea } from 'src/domain/mileage-management/mileage-area';
 
+const SEMESTER_OPTIONS = [
+  { value: '1', label: '1학기' },
+  { value: '2', label: '2학기' },
+  { value: '3', label: '하계 계절학기' },
+  { value: '4', label: '동계 계절학기' },
+];
+
+// 년도 옵션 생성
+const generateYearOptions = () => {
+  const currentYear = new Date().getFullYear();
+  const startYear = 2024;
+  const options = [];
+  for (let year = startYear; year <= currentYear; year += 1) {
+    options.push({ value: year.toString(), label: `${year}년` });
+  }
+  return options;
+};
+
+const YEAR_OPTIONS = generateYearOptions();
+
 interface DynamicFormProps {
   area: MileageArea;
 }
@@ -17,6 +37,62 @@ export const DynamicForm = ({ area }: DynamicFormProps) => {
   } = useFormContext();
 
   const renderField = (field: { name: string; type: string }) => {
+
+    if (field.name === '년도') {
+      return (
+        <Controller
+          key={field.name}
+          name={field.name}
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <TextField
+              label={field.name}
+              select
+              fullWidth
+              value={value || ''}
+              onChange={(e) => onChange(e.target.value)}
+              margin="normal"
+              error={Boolean(errors[field.name])}
+            >
+              {YEAR_OPTIONS.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          )}
+        />
+      );
+    }
+    
+    
+    if (field.name === '학기') {
+      return (
+        <Controller
+          key={field.name}
+          name={field.name}
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <TextField
+              label={field.name}
+              select
+              fullWidth
+              value={value || ''}
+              onChange={(e) => onChange(e.target.value)}
+              margin="normal"
+              error={Boolean(errors[field.name])}
+            >
+              {SEMESTER_OPTIONS.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          )}
+        />
+      );
+    }
+
     switch (field.type) {
       case 'number':
         return (
